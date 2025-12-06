2aca89522b9092a481440dcebd76cd0fafb8a078
celyk <humanimations10@gmail.com>
2025-12-06 17:53:21 -0600

    Boilerplate

--- importer.gd
+++ importer.gd
@@ -1,2 +1,17 @@
 @tool
 extends EditorImportPlugin
+
+func _get_importer_name() -> String:
+	return "ISFImporter"
+
+func _get_visible_name() -> String:
+	return "ISFImporter"
+
+func _get_recognized_extensions() -> PackedStringArray:
+	return [".fs", ".isf"]
+
+func _get_save_extension() -> String:
+	return ".isfimported"
+
+func _get_resource_type() -> String:
+	return "Shader"
