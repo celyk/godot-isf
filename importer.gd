a6f6aead8de6e40e849af61499eb3c07ce029a2f
celyk <humanimations10@gmail.com>
2025-12-06 18:09:17 -0600

    import plugin working minimally

--- importer.gd
+++ importer.gd
@@ -5,13 +5,13 @@ func _get_importer_name() -> String:
 	return "ISFImporter"
 
 func _get_visible_name() -> String:
-	return "ISFImporter"
+	return "ISF Importer"
 
 func _get_recognized_extensions() -> PackedStringArray:
 	return ["fs", "isf"]
 
 func _get_save_extension() -> String:
-	return ".isfimported"
+	return "material"
 
 func _get_resource_type() -> String:
 	return "ShaderMaterial"
