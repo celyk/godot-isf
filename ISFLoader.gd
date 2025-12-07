fe9379266657ae1edc6e7abb58d76e66ce75eb90
celyk <humanimations10@gmail.com>
2025-12-06 18:03:48 -0600

    Trying to get import to work

---
+++ ISFLoader.gd
@@ -0,0 +1,7 @@
+class_name ISFLoader extends RefCounted
+
+func _init() -> void:
+	pass
+
+func compile_shader() -> ShaderMaterial:
+	return preload("uid://cmagfpnocdt2s")
--- importer.gd
+++ importer.gd
@@ -8,10 +8,22 @@ func _get_visible_name() -> String:
 	return "ISFImporter"
 
 func _get_recognized_extensions() -> PackedStringArray:
-	return [".fs", ".isf"]
+	return ["fs", "isf"]
 
 func _get_save_extension() -> String:
 	return ".isfimported"
 
 func _get_resource_type() -> String:
-	return "Shader"
+	return "ShaderMaterial"
+
+func _get_import_options(path: String, preset_index: int) -> Array[Dictionary]:
+	return []
+
+func _import(source_file: String, save_path: String, options: Dictionary, platform_variants: Array[String], gen_files: Array[String]) -> Error:
+	var path_to_save : String = save_path + '.' + _get_save_extension()
+	
+	var loader := ISFLoader.new()
+	
+	var material : ShaderMaterial =  loader.compile_shader()
+	
+	return ResourceSaver.save(material, path_to_save)
