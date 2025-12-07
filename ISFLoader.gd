99ad593e72c28d100dc6b91afbd5d4a73091e553
celyk <humanimations10@gmail.com>
2025-12-06 18:26:06 -0600

    Start ISF include file

--- ISFLoader.gd
+++ ISFLoader.gd
@@ -1,7 +1,19 @@
 class_name ISFLoader extends RefCounted
 
+var file : FileAccess
+var vertex_shader_source : String
+var fragment_shader_source : String
+var json := {}
+
 func _init() -> void:
 	pass
 
-func compile_shader() -> ShaderMaterial:
+func create(path:String) -> ISFLoader:
+	file = FileAccess.open(path, FileAccess.READ)
+	
+	fragment_shader_source = file.get_as_text()
+	
+	return ISFLoader.new()
+
+func compile_shader(shader_type:=0) -> ShaderMaterial:
 	return preload("uid://cmagfpnocdt2s")
