11a358658ebf5b49390e6efcf767d941cedc5a4e
celyk <humanimations10@gmail.com>
2025-12-07 18:28:50 -0600

    Tweak

--- importer.gd
+++ importer.gd
@@ -1,14 +1,14 @@
 @tool
 extends EditorSceneFormatImporter
 
+enum SceneType {CONTROL, NODE_2D, NODE_3D}
+
 func _get_extensions() -> PackedStringArray:
 	return ["fs", "isf"]
 
-enum SceneType {CONTROL, NODE_2D, NODE_3D}
-
-var _extensions : PackedStringArray
+var _options : PackedStringArray
 func _add_import_option_helper(name: String, value: Variant, type:Variant.Type=TYPE_NIL, hint:PropertyHint=PROPERTY_HINT_NONE, hint_string:String="", usage_flags:int=6):
-	_extensions.append(name)
+	_options.append(name)
 	
 	if type == TYPE_NIL:
 		add_import_option(name, value)
@@ -21,21 +21,19 @@ func _get_import_options(path: String) -> void:
 
 # Remove all the built in options that we can
 func _get_option_visibility(path: String, for_animation: bool, option: String) -> Variant:
-	if option in _extensions: return null
+	if option in _options: return null
 	return false
 
 func _import_scene(source_file: String, flags: int, options: Dictionary) -> Object:
 	var scene_type : SceneType = options["scene_type"]
 	var scene_root := Node.new()
 	
-	#var path_to_save : String = save_path + '.' + _get_save_extension()
 	var loader := ISFLoader.create(source_file)
 	var material : ShaderMaterial =  loader.compile_shader()
 	material.resource_local_to_scene = true
 	
 	var mesh := QuadMesh.new()
 	
-	print("scene_type ", scene_type)
 	match scene_type:
 		SceneType.CONTROL:
 			scene_root = ColorRect.new()
