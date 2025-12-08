357122fa28276ce47d80e53a4ed93a64f16cd843
celyk <humanimations10@gmail.com>
2025-12-08 16:11:50 -0600

    Import as include

--- resource_importer.gd
+++ resource_importer.gd
@@ -11,10 +11,10 @@ func _get_recognized_extensions() -> PackedStringArray:
 	return ["fs", "isf"]
 
 func _get_save_extension() -> String:
-	return "tscn"
+	return "gdshaderinc"
 
 func _get_resource_type() -> String:
-	return "PackedScene"
+	return "ShaderInclude"
 
 func _get_import_options(path: String, preset_index: int) -> Array[Dictionary]:
 	return []
@@ -24,10 +24,23 @@ func _import(source_file: String, save_path: String, options: Dictionary, platfo
 	
 	var path_to_save : String = save_path + '.' + _get_save_extension()
 	
+	var file := FileAccess.open(source_file, FileAccess.READ) 
+	var source := file.get_as_text()
+	
+	
+	var shader_include := ShaderInclude.new()
+	shader_include.code = '#include "res://addons/godot-isf/include/ISF.gdshaderinc"\n\n' + source
+	#shader_include.code += source
+	
+	
+	
+	return ResourceSaver.save(shader_include, path_to_save)
+	
 	#var scene_type : ISFConverter.SceneType = options["scene_type"]
 	
 	var converter := ISFConverter.new()
 	var isf_file := ISFFile.open(source_file)
+	
 	
 	var scene_root := converter.convert_isf_to_scene(isf_file)
 	
